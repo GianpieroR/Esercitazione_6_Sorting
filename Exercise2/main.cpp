bf348448a44a04f821a4076d1f91d3a61cd9082c
GianpieroR <s308537@studenti.polito.it>
2025-04-25 17:29:25 +0200

    Svolgo gli esercizi

--- Exercise2/main.cpp
+++ Exercise2/main.cpp
@@ -1,5 +1,182 @@
-int main()
+#include <cmath> // contiene floor
+#include <iostream>
+#include <numeric> // contiene iota
+#include <iostream>
+#include <sstream>
+#include <chrono> // https://en.cppreference.com/w/cpp/chrono
+
+#include "SortingAlgorithm.hpp"
+using namespace std;
+
+template<typename T>
+string ArrayToString(const vector<T>& v)
 {
-    return 0;
+    string str;
+    ostringstream toString;
+    toString << "[ ";
+    for (const T& e: v)
+        toString << e << " ";
+    toString << "]";
+
+    return toString.str();
 }
 
+struct ClassObj{
+    double value;
+};
+
+inline bool operator< (const ClassObj& lhs, const ClassObj& rhs) { return lhs.value < rhs.value; }
+
+int main(int argc, char *argv[])
+{
+	unsigned int m;
+	istringstream st(argv[1]);
+	st >> m;
+    vector<int> v1(m);
+	
+	/* v1 is an ordered vector from -5 to m-5, where m is the size of the vector, so it is the best case possible because it's already sorted! */ 
+	
+	std::iota(v1.begin(),v1.end(),-5);
+
+    vector<double> v2(m);
+	
+	/* v2 is made of random elements between 0 and 1 */
+	
+	for(unsigned int i = 0; i<m; i++)
+		v2[i] = rand() / ((double)RAND_MAX); 
+
+    vector<int> v3(m, 0); 
+	
+	/* v3 is ordered in the first half, and made of random elements in the second half */
+	
+	copy(v1.begin(), v1.begin()+floor(m*0.5)+1,v3.begin()); 
+     for(unsigned int i = floor(m*0.5)+1; i<m;i++)
+		v3[i] = rand() % 1000; 
+	
+	/* Now, I create the worst case possible, by reversing vector v1 */
+
+	vector<int> v4(m);
+	for(unsigned int i = 0; i<m; i++)
+		v4[i] = v1[m-i-1];
+
+	/*I run 100 experiments and take the mean execution time in all the experiments */
+	
+    unsigned int num_experiment = 100;
+
+    double time_elapsed_bubble_v1 = 0.0;
+    for(unsigned int t = 0; t < num_experiment; t++)
+    {
+        vector<int> v(v1);
+
+        std::chrono::steady_clock::time_point start_time = std::chrono::steady_clock::now();
+        SortLibrary::BubbleSort<int>(v);
+        std::chrono::steady_clock::time_point end_time = std::chrono::steady_clock::now();
+        time_elapsed_bubble_v1 += std::chrono::duration_cast<std::chrono::microseconds>(end_time - start_time).count();
+    }
+    time_elapsed_bubble_v1 /= num_experiment;
+
+    cout << "Bubble Sort - v1 (best case): " << time_elapsed_bubble_v1 << endl;
+
+    double time_elapsed_bubble_v2 = 0.0;
+    for(unsigned int t = 0; t < num_experiment; t++)
+    {
+        vector<double> v(v2);
+
+        std::chrono::steady_clock::time_point start_time = std::chrono::steady_clock::now();
+        SortLibrary::BubbleSort<double>(v);
+        std::chrono::steady_clock::time_point end_time = std::chrono::steady_clock::now();
+        time_elapsed_bubble_v2 += std::chrono::duration_cast<std::chrono::microseconds>(end_time - start_time).count();
+    }
+    time_elapsed_bubble_v2 /= num_experiment;
+
+
+    cout << "Bubble Sort - v2 (random vector): " << time_elapsed_bubble_v2 << endl;
+
+
+    double time_elapsed_bubble_v3 = 0.0;
+    for(unsigned int t = 0; t < num_experiment; t++)
+    {
+        vector<int> v(v3);
+
+        std::chrono::steady_clock::time_point start_time = std::chrono::steady_clock::now();
+        SortLibrary::BubbleSort<int>(v);
+        std::chrono::steady_clock::time_point end_time = std::chrono::steady_clock::now();
+        time_elapsed_bubble_v3 += std::chrono::duration_cast<std::chrono::microseconds>(end_time - start_time).count();
+    }
+    time_elapsed_bubble_v3 /= num_experiment;
+
+    cout << "Bubble Sort - v3 (mixed case): " << time_elapsed_bubble_v3 << endl;
+	
+	double time_elapsed_bubble_v4 = 0.0;
+    for(unsigned int t = 0; t < num_experiment; t++)
+    {
+        vector<int> v(v4);
+
+        std::chrono::steady_clock::time_point start_time = std::chrono::steady_clock::now();
+        SortLibrary::BubbleSort<int>(v);
+        std::chrono::steady_clock::time_point end_time = std::chrono::steady_clock::now();
+        time_elapsed_bubble_v4 += std::chrono::duration_cast<std::chrono::microseconds>(end_time - start_time).count();
+    }
+    time_elapsed_bubble_v4 /= num_experiment;
+
+    cout << "Bubble Sort - v4 (worst case): " << time_elapsed_bubble_v4 << endl;
+	
+	double time_elapsed_heap_v1 = 0.0;
+    for(unsigned int t = 0; t < num_experiment; t++)
+    {
+        vector<int> v(v1);
+
+        std::chrono::steady_clock::time_point start_time = std::chrono::steady_clock::now();
+        SortLibrary::HeapSort<int>(v);
+        std::chrono::steady_clock::time_point end_time = std::chrono::steady_clock::now();
+        time_elapsed_heap_v1 += std::chrono::duration_cast<std::chrono::microseconds>(end_time - start_time).count();
+    }
+    time_elapsed_heap_v1 /= num_experiment;
+
+    cout << "Heap Sort - v1 (Best case): " << time_elapsed_heap_v1 << endl;
+
+    double time_elapsed_heap_v2 = 0.0;
+    for(unsigned int t = 0; t < num_experiment; t++)
+    {
+        vector<double> v(v2);
+
+        std::chrono::steady_clock::time_point start_time = std::chrono::steady_clock::now();
+        SortLibrary::HeapSort<double>(v);
+        std::chrono::steady_clock::time_point end_time = std::chrono::steady_clock::now();
+        time_elapsed_heap_v2 += std::chrono::duration_cast<std::chrono::microseconds>(end_time - start_time).count();
+    }
+    time_elapsed_heap_v2 /= num_experiment;
+
+    cout << "Heap Sort - v2 (Random case): " << time_elapsed_heap_v2 << endl;
+
+
+    double time_elapsed_heap_v3 = 0.0;
+    for(unsigned int t = 0; t < num_experiment; t++)
+    {
+        vector<int> v(v3);
+
+        std::chrono::steady_clock::time_point start_time = std::chrono::steady_clock::now();
+        SortLibrary::HeapSort<int>(v);
+        std::chrono::steady_clock::time_point end_time = std::chrono::steady_clock::now();
+        time_elapsed_heap_v3 += std::chrono::duration_cast<std::chrono::microseconds>(end_time - start_time).count();
+    }
+    time_elapsed_heap_v3 /= num_experiment;
+
+    cout << "Heap Sort - v3 (Mixed case): " << time_elapsed_heap_v3 << endl;
+	
+	double time_elapsed_heap_v4 = 0.0;
+    for(unsigned int t = 0; t < num_experiment; t++)
+    {
+        vector<int> v(v4);
+
+        std::chrono::steady_clock::time_point start_time = std::chrono::steady_clock::now();
+        SortLibrary::BubbleSort<int>(v);
+        std::chrono::steady_clock::time_point end_time = std::chrono::steady_clock::now();
+        time_elapsed_heap_v4 += std::chrono::duration_cast<std::chrono::microseconds>(end_time - start_time).count();
+    }
+    time_elapsed_heap_v4 /= num_experiment;
+
+    cout << "Heap Sort - v4 (worst case): " << time_elapsed_heap_v4 << endl;
+	
+    return 0;
+}
--- Exercise2/src_test/test_sorting_algorithm.hpp
+++ Exercise2/src_test/test_sorting_algorithm.hpp
@@ -8,20 +8,150 @@
 
 namespace SortLibrary {
 
-TEST(TestSorting, TestSelectionSort)
+/* Here, I test the bubble sort and the heap sort with vectors of int and doubles */
+
+TEST(TestSorting, BubbleSortWithInt)
 {
-    std::vector<int> v = {9, 13, 5, 10, 2, 7, 9, 4, 6, 12};
-    SelectionSort<int>(v);
-    std::vector<int> sortedV = {2, 4, 5, 6, 7, 9, 9, 10, 12 ,13};
+    vector<int> v = {9, 13, 5, 10, 2, 7, 9, 4, 6, 12};
+    BubbleSort<int>(v);
+    vector<int> sortedV = {2, 4, 5, 6, 7, 9, 9, 10, 12 ,13};
     EXPECT_EQ(v, sortedV);
 }
 
-TEST(TestSorting, TestInsertionSort)
+TEST(TestSorting, BubbleSortWithDouble)
 {
-    std::vector<int> v = {9, 13, 5, 10, 2, 7, 9, 4, 6, 12};
-    InsertionSort<int>(v);
-    std::vector<int> sortedV = {2, 4, 5, 6, 7, 9, 9, 10, 12 ,13};
+	vector<double> v = {12.3, -14.0, 1.7e-5, 1.8e+12, 35.3555, 12.0, 0.0};
+	vector<double> sortedV = {-14.0, 0.0, 1.7e-5, 12.0, 12.3, 35.3555, 1.8e+12};
+	BubbleSort<double>(v);
+	EXPECT_EQ(v,sortedV);
+}
+	
+TEST(TestSorting, HeapSortWithInt)
+{
+    vector<int> v = {9, 13, 5, 10, 2, 7, 9, 4, 6, 12};
+    HeapSort<int>(v);
+    vector<int> sortedV = {2, 4, 5, 6, 7, 9, 9, 10, 12 ,13};
     EXPECT_EQ(v, sortedV);
+}
+
+TEST(TestSorting, HeapSortWithDouble)
+{
+	vector<double> v = {12.3, -14.0, 1.7e-5, 1.8e+12, 35.3555, 12.0, 0.0};
+	vector<double> sortedV = {-14.0, 0.0, 1.7e-5, 12.0, 12.3, 35.3555, 1.8e+12};
+	HeapSort<double>(v);
+	EXPECT_EQ(v,sortedV);
+}
+
+/* Now, I run some tests of the two algorithms using the best and the worst case */
+
+TEST(TestSorting, BubbleSortBestCase)
+{
+	vector<double> v(100);
+	for(int i = 0; i<100; i++)
+		v[i] = 3*i + 4;
+	
+	/* This is just a copy of v inside sortedV, so if 
+	something goes wrong inside the BubbleSort I can see it by 
+	comparing v with this original copy (because v should remain equal to the 
+	original copy in the best case) */
+	
+	vector<double> sortedV = v;
+	BubbleSort(v);
+	EXPECT_EQ(v, sortedV);
+}
+
+TEST(TestSorting, BubbleSortWorstCase)
+{
+	vector<double> v(100);
+	
+	/*I initialize v in decreasing order */
+	
+	for(int i = 0; i<100; i++)
+		v[i] = -4*i;
+	
+	/*Now i put in sortedV the reversed vector v */
+	
+	vector<double> sortedV(100);
+	for(int i = 0; i<100; i++)
+		sortedV[i] = v[100-i-1];
+	BubbleSort(v);
+	
+	/*I expect v after the bubblesort to be equal to its reversed values */
+	
+	EXPECT_EQ(v, sortedV);
+}
+
+TEST(TestSorting, HeapSortBestCase)
+{
+	vector<double> v(100);
+	for(int i = 0; i<100; i++)
+		v[i] = 3*i + 4;
+	vector<double> sortedV = v;
+	HeapSort(v);
+	EXPECT_EQ(v, sortedV);
+}
+
+TEST(TestSorting, HeapSortWorstCase)
+{
+	vector<double> v(100);
+	for(int i = 0; i<100; i++)
+		v[i] = -4*i;
+	vector<double> sortedV(100);
+	for(int i = 0; i<100; i++)
+		sortedV[i] = v[100-i-1];
+	HeapSort(v);
+	EXPECT_EQ(v, sortedV);
+}
+
+/*Here, an idea should be to consider a randomized vector of 500 elements, and 
+then create its copy, apply the two different methods on the vectors, and if the 
+two algorithms work correctly, I should expect an equivalence between the two results, 
+because the starting vector is the same */
+
+TEST(TestSorting, HeapSort_and_BubbleSort)
+{
+	vector<double> v(500);
+	vector<double> w(500);
+	for(unsigned int i = 0; i<500; i++)
+		v[i] = rand();
+	w = v;
+	BubbleSort<double>(v);
+	HeapSort<double>(w);
+	EXPECT_EQ(v,w);
+}
+
+/* Here, I test the case of HeapSort and BubbleSort with size 1 vectors and void vectors */
+ 
+TEST(TestSorting, HeapSortSizeOne)
+{
+	vector<double> v = {4.0};
+	vector<double> sortedV = {4.0};
+	HeapSort(v);
+	EXPECT_EQ(v, sortedV);
+}
+
+TEST(TestSorting, BubbleSortSizeOne)
+{
+	vector<double> v = {2.0};
+	vector<double> sortedV = {2.0};
+	BubbleSort(v);
+	EXPECT_EQ(v, sortedV);
+}
+
+TEST(TestSorting, HeapSortVoidVector)
+{
+	vector<double> v;
+	vector<double> sortedV;
+	HeapSort(v);
+	EXPECT_EQ(v, sortedV);
+}
+
+TEST(TestSorting, BubbleSortVoidVector)
+{
+	vector<double> v;
+	vector<double> sortedV;
+	BubbleSort(v);
+	EXPECT_EQ(v, sortedV);
 }
 
 }
